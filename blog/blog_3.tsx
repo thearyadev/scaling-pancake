6e21cddf7e7ca0b91a71f3b06686390aaa5224b5
Aryan Kothari <personal@aryankothari.dev>
2023-12-27 23:32:30 +0000

    add starter blog post

---
+++ blog/blog_3.tsx
@@ -0,0 +1,16 @@
+import {PostMetadata} from "@/models";
+
+export const metadata: PostMetadata = {
+    title: "",
+    subtitle: "",
+    description: "",
+    date: "Wed Dec 27 2023 23:28:35 GMT+0000 (Coordinated Universal Time)"
+}
+
+const BlogPostContent = () => {
+    return <>
+        {metadata.title}
+    </>
+}
+
+export default BlogPostContent
