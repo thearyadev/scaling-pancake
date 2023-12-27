b70e283fbb6054bb8f8476bf3bd909cc84076489
Aryan Kothari <personal@aryankothari.dev>
2023-12-27 21:33:54 +0000

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+import Image from "next/image";
 export default function Home() {
   return (
-      <>
-        <h1>homepage</h1>
-      </>
-  )
+    <>
+      <h1>homepage</h1>
+    </>
+  );
 }
