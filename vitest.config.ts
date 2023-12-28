dcd1a2a6b89e419d7268e86b669ad115a4402faf
Aryan Kothari <personal@aryankothari.dev>
2023-12-28 23:32:19 +0000

    add import alias for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'jsdom',
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@/": new URL("./", import.meta.url).pathname,
     },
-    resolve:{
-        alias: {
-            '@/': new URL('./', import.meta.url).pathname,
-        }
-    }
-})
+  },
+});
