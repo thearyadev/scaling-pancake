0a324c13ef9eb1531d67200adbcb46433e2b243d
Aryan Kothari <personal@aryankothari.dev>
2023-12-28 23:28:31 +0000

    add testing configs

---
+++ tests/app/page.test.tsx
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Home from "@/app/page";
+
+
+test('Page', () => {
+    render(<Home />)
+    expect(screen.getByRole('heading', { level: 1, name: 'homepage' })).toBeDefined()
+})
