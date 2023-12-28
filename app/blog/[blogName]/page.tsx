55dde2a457e71b30e56b49810d3ea270d2912858
Aryan Kothari <personal@aryankothari.dev>
2023-12-28 05:41:56 +0000

    remove loader from local file

--- app/blog/[blogName]/page.tsx
+++ app/blog/[blogName]/page.tsx
@@ -1,20 +1,14 @@
-import {Post} from "@/models";
+import {SinglePostLoader} from "@/lib/postLoader";
+import {notFound} from "next/navigation";
 
-function get_post_from_mod_name(modName: string): Post{
-    const module = require(`../../../blog/${modName}`)
-    return {
-        metadata: module.metadata,
-        component: module.default,
-        modName: modName
+const BlogPost = ({params}: { params: { blogName: string } }) => {
+    try {
+        const Post = SinglePostLoader("content/blog", params.blogName)[0]
+        return <>
+            <Post.component/>
+        </>
+    } catch (error) {
+        notFound()
     }
 }
-
-const BlogPost = ({params}:  {params: {blogName: string}}) => {
-    const Post = get_post_from_mod_name(params.blogName)
-    return <>
-        <Post.component />
-    </>
-
-}
-
 export default BlogPost
