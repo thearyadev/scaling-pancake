51b1e4dc36846aaec72c2b5867767428ef46e64a
Aryan Kothari <personal@aryankothari.dev>
2023-12-28 23:33:55 +0000

    add homepage test placeholder

--- tests/app/page.test.tsx
+++ tests/app/page.test.tsx
@@ -1,9 +1,10 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
-
-test('Page', () => {
-    render(<Home />)
-    expect(screen.getByRole('heading', { level: 1, name: 'homepage' })).toBeDefined()
-})
+test("Page", () => {
+  render(<Home />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "homepage" }),
+  ).toBeDefined();
+});
