39499dc8b244401cf8dfa182f63a19c66ebff6ec
Aryan Kothari <personal@aryankothari.dev>
2023-12-28 05:41:34 +0000

    add projects page

---
+++ app/projects/[projectName]/page.tsx
@@ -0,0 +1,14 @@
+import {SinglePostLoader} from "@/lib/postLoader";
+import {notFound} from "next/navigation";
+
+const ProjectsPost = ({params}: { params: { projectName: string } }) => {
+    try {
+        const Post = SinglePostLoader("content/projects", params.projectName)[0]
+        return <>
+            <Post.component/>
+        </>
+    } catch (error) {
+        notFound()
+    }
+}
+export default ProjectsPost
