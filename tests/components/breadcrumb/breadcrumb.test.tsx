95704cd19402dc91860c962fb13e509b0d455545
Aryan Kothari <personal@aryankothari.dev>
2023-12-29 04:44:07 +0000

    add breadcrumb test

---
+++ tests/components/breadcrumb/breadcrumb.test.tsx
@@ -0,0 +1,28 @@
+import { test, expect, vi } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import Breadcrumb from "@/components/breadcrumb/breadcrumb";
+
+test("breadcrumb path with leading forward slash", () => {
+  vi.mock("next/navigation", () => ({
+    usePathname() {
+      return "/blog/gnome";
+    },
+  }));
+  const { container } = render(<Breadcrumb />);
+  expect(container.textContent).toBe("blog/gnome/");
+  cleanup();
+});
+
+test("breadcrumb path without leading forward slash", () => {
+  vi.mock("next/navigation", () => ({
+    usePathname() {
+      return "blog/gnome";
+    },
+  }));
+  const { container } = render(<Breadcrumb />);
+  console.log(container.textContent);
+  expect(container.textContent).toBe("blog/gnome/");
+  cleanup();
+});
+
+test.todo("link is functional");
