247cc6af86ff7cc76e995c836b8d6960cd62016f
Aryan Kothari <personal@aryankothari.dev>
2023-12-29 04:40:48 +0000

    add breadcrumb to layout (temporary)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { redirect } from "next/navigation";
+import Breadcrumb from "@/components/breadcrumb/breadcrumb";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Breadcrumb />
         {children}
 
         <a className="text-bold m-10 p-10 text-2xl" href="/">
