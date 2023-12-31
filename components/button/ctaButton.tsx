55e7e5a4da08e5f5382fb31e9a79cce96fd952fc
Aryan Kothari <personal@aryankothari.dev>
2023-12-31 04:56:02 +0000

    add classname prop for external style def

--- components/button/ctaButton.tsx
+++ components/button/ctaButton.tsx
@@ -4,14 +4,20 @@ import Link from "next/link";
 
 const CallToActionButton = ({
     children,
+    className,
     redirectTo,
 }: {
     children: React.ReactNode;
+    className?: string;
     redirectTo: string;
 }) => {
     return (
         <Link href={redirectTo}>
-            <Button variant="outline" size="sm" className="border-black">
+            <Button
+                variant="outline"
+                size="sm"
+                className={`border-black ${className}`}
+            >
                 {children}
             </Button>
         </Link>
